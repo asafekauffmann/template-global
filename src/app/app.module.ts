dbcab10d08ce0bc40c0d0830058f54ba63980169
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-12-02 16:52:23 -0300

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TestComponent } from './test/test.component';
 import { BootModelComponent } from './boot-model/boot-model.component';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { BootModelComponent } from './boot-model/boot-model.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    TabsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
