143c2ef1edb39ed604afc591d28c6dd3a990d887
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-12-03 14:25:41 -0300

    up

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+   selector: 'app-modal',
+   templateUrl: './modal.component.html',
+   styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+   modalRef?: BsModalRef;
 
-  ngOnInit(): void {
-  }
+   constructor(private modalService: BsModalService) { }
+
+   ngOnInit(): void {
+   }
+
+   openModalWithClass(template: TemplateRef<any>) {
+      this.modalRef = this.modalService.show(
+        template,
+        Object.assign({}, { class: 'gray modal-lg' })
+      );
+    }
 
 }
